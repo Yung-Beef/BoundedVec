4a51b4d6db4b56ea38f22bb93fd39126ffa8de21
Yung-Beef <yungbeefbigbags@gmail.com>
2024-10-11 09:59:40 -0600

    added push and pop and fixed new

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 //type BoundedVec<u8, ConstU32<100>>
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
-struct BoundedVec<T, U> {
+struct BoundedVec<T> {
     data: Vec<T>,
-    limit: U,
+    limit: i32,
 }
 
-impl<T, U> BoundedVec<T, U>
-where
-    U: Into<usize> + Clone + std::cmp::PartialOrd<i32>,
+impl<T> BoundedVec<T>
 {
-    fn new(limit: U) -> Result<BoundedVec<T, U>, &'static str> {
-        if limit < 1 {
-            Err("Cannot create a zero length vector")
+    fn new(limit: i32) -> Result<BoundedVec<T>, &'static str> {
+        if limit < 0 {
+            Err("Cannot create a negative length vector")
         } else {
             Ok(BoundedVec {
                 data: Vec::new(),
@@ -21,8 +19,8 @@ where
         }
     }
 
-    fn try_from_vec<V: Clone>(v: &Vec<V>, limit: U) -> Result<BoundedVec<V, U>, &'static str> {
-        let length: usize = limit.clone().into();
+    fn try_from_vec<V: Clone>(v: &Vec<V>, limit: i32) -> Result<BoundedVec<V>, &'static str> {
+        let length: usize = limit.clone().try_into().expect("i32 should always convert to usize");
         if v.len() > length {
             Err("Vector too long")
         } else {
@@ -32,14 +30,53 @@ where
             })
         }
     }
+
+    fn push(&mut self, data: T) -> Result<(), &'static str> {
+        let limit: usize = self.limit.clone().try_into().expect("i32 should always convert to usize");
+        if self.data.len() >= limit {
+            Err("Vector full")
+        } else {
+            self.data.push(data);
+            Ok(())
+        }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.data.pop()
+    }
 }
 
 fn main() {}
 
 #[test]
+fn test_new() {
+    let l = -5;
+    assert_eq!(Err("Cannot create a negative length vector"), BoundedVec::<i8>::new(l));
+    let l = 5;
+    assert_eq!(Ok(BoundedVec { data: Vec::new(), limit: 5 }), BoundedVec::<i8>::new(l));
+}
+
+#[test]
 fn test_try_from_vec() {
     let v: Vec<i32> = vec![1, 2, 3];
-    let test = BoundedVec::<i32, u16>::try_from_vec(&v, 4u16);
+    let test = BoundedVec::<i32>::try_from_vec(&v, 4);
     println!("{:?}", test);
     assert_eq!(test, Ok(BoundedVec { data: v, limit: 4 }));
+}
+
+#[test]
+fn test_push() {
+    let mut vec = BoundedVec::<i8>::new(5).unwrap();
+    for _ in 0..5 {
+        let _ = vec.push(1);
+    }
+    assert_eq!(vec, BoundedVec { data: vec![1, 1, 1, 1, 1], limit: 5 });
+    assert_eq!(vec.push(1), Err("Vector full"));
+}
+
+#[test]
+fn test_pop() {
+    let mut vec = BoundedVec::<i8>::new(5).unwrap();
+    let _ = vec.push(5);
+    assert_eq!(vec.pop(), Some(5));
 }
